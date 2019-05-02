b8d0f23bc90a8a8e2ab3d89c59f6f2d6034874be
Grammy Jiang <grammy.jiang@gmail.com>
2019-05-02 12:29:15 +1000

    Initial this repo with a scrapy project structure
    This commit adds the scrapy project structure into this repo

---
+++ r18/settings/__init__.py
@@ -0,0 +1,24 @@
+BOT_NAME = 'r18'
+
+SPIDER_MODULES = ['r18.spiders']
+NEWSPIDER_MODULE = 'r18.spiders'
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = True
+
+# Disable Telnet Console (enabled by default)
+# TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+# DEFAULT_REQUEST_HEADERS = {
+#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#   'Accept-Language': 'en',
+# }
+
+SPIDER_MIDDLEWARES = {}
+
+DOWNLOADER_MIDDLEWARES = {}
+
+EXTENSIONS = {}
+
+ITEM_PIPELINES = {}
