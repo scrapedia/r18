be35b8627c04cca4a86351d51f0b43477f270d58
Grammy Jiang <grammy.jiang@gmail.com>
2019-05-14 13:04:52 +1000

    Update item processors and Pipfile
    This commit adds:
    
     * Update item processors to fit MongoDB indexes
     * Add pylint into Pipfile dev section

--- r18/items.py
+++ r18/items.py
@@ -1,44 +1,62 @@
+import re
+from datetime import datetime
 from typing import Dict, Generator, Iterable, List, Tuple
 
 from scrapy.item import Field, Item
 from scrapy.loader.processors import TakeFirst
 from scrapy.selector import Selector
 
+PATTERN_RUNTIME = re.compile(
+    r"(?P<runtime>\d+)min\.(\s\(HD:\s(?P<hd_runtime>\d+)min\.\))?", flags=re.VERBOSE
+)
+
 
 class ParseActresses(object):
     def __call__(self, actresses: List[str]) -> Generator[Tuple[str, str], None, None]:
         for _ in actresses:
             href, text = Selector(text=_).css("a::attr(href), span::text").extract()
-            yield text.strip(), href
+            yield {"name": text.strip(), "url": href}
 
 
 class ParseCategories(object):
     def __call__(self, categories: List[str]) -> Generator[Tuple[str, str], None, None]:
         for _ in categories:
             href, text = Selector(text=_).css("a::attr(href), a::text").extract()
-            yield text.strip(), href
+            yield {"name": text.strip(), "url": href}
 
 
 class ParseDetail(object):
     def __init__(self):
         self.product_parser = {
             "Channel": self._parse_channel,
+            "Release Date": self._release_date,
             "Runtime": self._parse_runtime,
             "Series": self._parse_series,
             "Studio": self._parse_studio,
         }
 
-    def _parse_channel(self, channel: Selector) -> Dict[str, str]:
-        _channel = dict()
+    def _parse_channel(self, channel: Selector) -> List[Dict[str, str]]:
+        _channel = list()
         for text, href in zip(
             channel.css("a::text").extract(), channel.css("a::attr(href)").extract()
         ):
-            _channel.update({text.strip(): href})
+            _channel.append({"name": text.strip(), "url": href})
         return _channel
+
+    def _release_date(self, release_date: Selector) -> datetime:
+        _release_date = release_date.css("dd::text").get().strip()
+        replace_pair = (("june", "jun"), ("july", "jul"), ("sept", "sep"))
+        _ = _release_date.lower()
+        for pair in replace_pair:
+            _ = _.replace(*pair)
+        try:
+            return datetime.strptime(_, "%b. %d, %Y")
+        except ValueError:
+            return datetime.strptime(_, "%b %d, %Y")
 
     def _parse_runtime(self, runtime: Selector):
         _runtime = " ".join(runtime.css("dd::text").get().strip().split())
-        return _runtime
+        return int(PATTERN_RUNTIME.match(_runtime).group("runtime"))
 
     def _parse_series(self, series: Selector):
         _series = dict()
@@ -48,12 +66,12 @@ class ParseDetail(object):
             _series.update({text.strip(): href})
         return _series
 
-    def _parse_studio(self, studio: Selector):
-        _studio = dict()
+    def _parse_studio(self, studio: Selector) -> List[Dict[str, str]]:
+        _studio = list()
         for text, href in zip(
             studio.css("a::text").extract(), studio.css("a::attr(href)").extract()
         ):
-            _studio.update({text.strip(): href})
+            _studio.append({"name": text.strip(), "url": href})
         return _studio
 
     def __call__(self, v: List[str]) -> Generator[Dict[str, str], None, None]:
@@ -93,5 +111,5 @@ class R18DetailItem(Item):
     image_detail_view = Field()
 
     detail = Field(input_processor=ParseDetail(), output_processor=JoinDict())
-    actresses = Field(input_processor=ParseActresses(), output_processor=dict)
-    categories = Field(input_processor=ParseCategories(), output_processor=dict)
+    actresses = Field(input_processor=ParseActresses())
+    categories = Field(input_processor=ParseCategories())
