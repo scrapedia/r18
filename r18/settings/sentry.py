fdd5b976f26dfd3deb495b9e075af2423742ff15
Grammy Jiang <gjiang@buildingsalive.com>
2019-11-27 14:08:56 +1100

    Black r18.settings.sentry module

--- r18/settings/sentry.py
+++ r18/settings/sentry.py
@@ -5,6 +5,5 @@ from sentry_sdk.integrations.logging import LoggingIntegration
 
 sentry_logging = LoggingIntegration(level=logging.INFO, event_level=logging.WARNING)
 sentry_sdk.init(
-    dsn="http://<public-key>@<sentry>/<project-id>",
-    integrations=[sentry_logging],
+    dsn="http://<public-key>@<sentry>/<project-id>", integrations=[sentry_logging]
 )
